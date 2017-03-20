bae48ece6facc7b0639065ccd31a3db2f675e155
Bili Xu <xubili93@gmail.com>
2017-03-21 00:34:39 +0800

    Remove PlayerSkeleton's run method

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -6,25 +6,6 @@ public class PlayerSkeleton {
         return 0;
     }
 
-    public int run() {
-        State s = new State();
-        new TFrame(s);
-        PlayerSkeleton p = new PlayerSkeleton();
-        while(!s.hasLost()) {
-            s.makeMove(p.pickMove(s,s.legalMoves()));
-            s.draw();
-            s.drawNext(0,0);
-            try {
-                Thread.sleep(300);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.out.println("You have completed "+s.getRowsCleared()+" rows.");
-        return s.getRowsCleared();
-    }
-
     public static void main(String[] args) {
         State s = new State();
         new TFrame(s);
