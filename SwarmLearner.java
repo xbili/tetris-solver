740200c329dcc501b4ddccc854660a016ad84117
Bili Xu <xubili93@gmail.com>
2017-03-21 00:20:05 +0800

    Implement stubbed SwarmLearner
    Refer to SwarmLearner to have a rough idea of how to implement your
    Learner.

---
+++ SwarmLearner.java
@@ -0,0 +1,22 @@
+public class SwarmLearner extends Learner {
+
+    @Override
+    protected int[] learn(int[] weights) {
+        // TODO: Stubbed
+        return new int[10];
+    }
+
+    // Test method run the learner
+    public static void main(String[] args) {
+        Learner learner = new SwarmLearner();
+
+        int[] dummy = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] result = learner.start(dummy, 100);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+
+}
+
