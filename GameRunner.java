acf9e312281b8de739af4a6521e84aeedb61e30e
Linh Nguyen Cao Nhat <nhatlinh95@gmail.com>
2017-03-31 17:06:55 +0800

    Update GameRunner java bridge

--- GameRunner.java
+++ GameRunner.java
@@ -1,45 +1,16 @@
 /**
- * Game runner class that takes input from STDIN, run the game and print result to STDOUT
+ * Game Runner class that takes in Input from STDIN and output result to STDOUT
  */
-public abstract class GameRunner {
+public class GameRunner {
 
-    private boolean display = false;
-
-    /**
-     * Runs the learning algorithm until convergence or max iteration stated
-     * is reached.
-     *
-     * @return optimized weight after learning has taken place
-     */
-    public int[] start(int[] initialWeights, int maxIterations) {
-        int iterations = maxIterations;
-        int[] weights = initialWeights;
-        while (iterations > 0) {
-            weights = learn(weights);
-
-            // TODO: Check for convergence of weights, we can leave this out
-            // for now first.
-
-            iterations--;
-        }
-
-        return weights;
-    }
-
-    /**
-     * Set display setting to true if you want to see the game being
-     * played in each iteration of the learning.
-     */
-    public void setDisplay(boolean display) {
-        this.display = display;
-    }
+    private static boolean display = false;
 
     /**
      * Runs the game using the specified weights.
      *
      * @return number of blocks cleared by the agent
      */
-    protected int run(State state, int[] weights) {
+    private static float run(State state, float[] weights) {
         PlayerSkeleton player = new PlayerSkeleton();
 
         // Create new display frame only if display setting is true
@@ -54,39 +25,9 @@ public abstract class GameRunner {
     }
 
     /**
-     * Calculates the utility value with the specified weights.
-     *
-     * NOTE: The first element of `features` need to be equal to 1 in order to
-     * take into account `w(0)` (the zero-th weight) for our utility function.
-     *
-     * @return utility value obtained from the weights
-     */
-    protected int getUtilityValue(int[] weights, int[] features) {
-        // Zero-th feature should be equal to 1
-        assert features[0] == 1;
-
-        // Weights and features should have the same length
-        assert weights.length == features.length;
-
-        int result = 0;
-        for (int i = 0; i < weights.length; i++) {
-            result += weights[i] * features[i];
-        }
-
-        return result;
-    }
-
-    /**
-     * XLearner class should, and needs to override this method.
-     *
-     * @return updated weight after ONE iteration of the learning algorithm
-     */
-    protected abstract int[] learn(int[] weights);
-
-    /**
      * Draws updated state onto the TFrame
      */
-    private void draw(State state) {
+    private static void draw(State state) {
         state.draw();
         state.drawNext(0, 0);
         try {
@@ -96,5 +37,13 @@ public abstract class GameRunner {
         }
     }
 
+    public static void main(String[] args) {
+        float weights[] = new float[args.length];
+        for(int i = 0; i < args.length; i++) {
+            weights[i] = Float.parseFloat(args[i]);
+        }
+        float result = run(new State(), weights);
+        System.out.printf("%0.2f\n", result);
+    }
 }
 
