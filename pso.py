5812436a91967723d9ff0992aced1617995b7353
Bili Xu <xubili93@gmail.com>
2017-04-01 11:25:19 +0800

    Implement stubbed swarm optmization methods

--- pso.py
+++ pso.py
@@ -1,8 +1,16 @@
 from pyswarm import pso
+from game_runner import one_iter
 
 # x is a vector of features
 def utility(x):
-    return sum(x)
+    # Convert all arguments to string
+    args = [str(arg) for arg in x]
+
+    # Lines cleared
+    cleared = one_iter(args)
+    print('Lines cleared: {}'.format(cleared))
+
+    return cleared
 
 def con(x):
     x1 = x[0]
@@ -10,11 +18,16 @@ def con(x):
     return [-(x1 + 0.25)**2 + 0.75*x2]
 
 def main():
-    lb = [-3, -1]
-    ub = [2, 6]
+    # Lower bound
+    lb = [-100] * 21
+
+    # Upper bound
+    ub = [100] * 21
 
     xopt, fopt = pso(utility, lb, ub, f_ieqcons=con)
 
     print(xopt)
     print(fopt)
 
+if __name__ == '__main__':
+    main()
