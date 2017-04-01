6408223a3b763c6536fc33746070660ea492a39f
Linh Nguyen Cao Nhat <nhatlinh95@gmail.com>
2017-04-01 15:27:25 +0800

    doesnt work anw

--- bayesian_learner.py
+++ bayesian_learner.py
@@ -3,14 +3,17 @@ import random
 from game_runner import GameRunner
 gr = GameRunner()
 
-def game_runner_evaluate(col1, col2, col3, col4, col5, col6, col7, col8, col9, col10,
+def game_runner_evaluate(
+						# col1, col2, col3, col4, col5, col6, col7, col8, col9, col10,
 						h_diff1, h_diff2, h_diff3, h_diff4, h_diff5, h_diff6, h_diff7, h_diff8, h_diff9,
-						num_holes):
+						highest_col, num_holes, num_holes_made, num_lines_cleared):
 	params = []
-	params.extend([col1, col2, col3, col4, col5, col6, col7, col8, col9, col10,
-						h_diff1, h_diff2, h_diff3, h_diff4, h_diff5, h_diff6, h_diff7, h_diff8, h_diff9])
-	params.extend([max(col1, col2, col3, col4, col5, col6, col7, col8, col9, col10)])
+	# params.extend([col1, col2, col3, col4, col5, col6, col7, col8, col9, col10])
+	params.extend([h_diff1, h_diff2, h_diff3, h_diff4, h_diff5, h_diff6, h_diff7, h_diff8, h_diff9])
+	params.extend([highest_col])
 	params.extend([num_holes])
+	params.extend([num_holes_made])
+	params.extend([num_lines_cleared])
 	to_display = True
 	# for num in params:
 	# 	print(num)
@@ -18,26 +21,30 @@ def game_runner_evaluate(col1, col2, col3, col4, col5, col6, col7, col8, col9, c
 	lines_cleared = gr.run(params, display=to_display)
 	return lines_cleared
 
-grBO = BayesianOptimization(game_runner_evaluate,{'col1': (0,1),
-												  'col2': (0,1),
-												  'col3': (0,1),
-												  'col4': (0,1),
-												  'col5': (0,1),
-												  'col6': (0,1),
-												  'col7': (0,1),
-												  'col8': (0,1),
-												  'col9': (0,1),
-												  'col10': (0,1),
-												  'h_diff1': (0,1),
-												  'h_diff2': (0,1),
-												  'h_diff3': (0,1),
-												  'h_diff4': (0,1),
-												  'h_diff5': (0,1),
-												  'h_diff6': (0,1),
-												  'h_diff7': (0,1),
-												  'h_diff8': (0,1),
-												  'h_diff9': (0,1),
-												  'num_holes': (0,1),
+grBO = BayesianOptimization(game_runner_evaluate,{
+													# 'col1': (-100,100),
+												 #  'col2': (-100,100),
+												 #  'col3': (-100,100),
+												 #  'col4': (-100,100),
+												 #  'col5': (-100,100),
+												 #  'col6': (-100,100),
+												 #  'col7': (-100,100),
+												 #  'col8': (-100,100),
+												 #  'col9': (-100,100),
+												 #  'col10': (-100,100),
+												  'h_diff1': (-100,100),
+												  'h_diff2': (-100,100),
+												  'h_diff3': (-100,100),
+												  'h_diff4': (-100,100),
+												  'h_diff5': (-100,100),
+												  'h_diff6': (-100,100),
+												  'h_diff7': (-100,100),
+												  'h_diff8': (-100,100),
+												  'h_diff9': (-100,100),
+												  'highest_col': (-100,100),
+												  'num_holes': (-100,100),
+												  'num_holes_made': (-100,100),
+												  'num_lines_cleared': (-100,100),
 												  })
 grBO.maximize(init_points = 5,  n_iter=15)
 
