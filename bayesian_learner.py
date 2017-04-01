3a8380cf34a44d323fb602c50272b8e8cefd7861
Sritam Patnaik <sritampatnaik@gmail.com>
2017-04-01 10:50:32 +0800

    changed weight ranges to 0-1

--- bayesian_learner.py
+++ bayesian_learner.py
@@ -15,30 +15,28 @@ def game_runner_evaluate(col1, col2, col3, col4, col5, col6, col7, col8, col9, c
 	# 	print(num)
 
 	lines_cleared = gr.run(params, display=to_display)
-	# lines_cleared = random.randint(1, 10)
-
 	return lines_cleared
 
-grBO = BayesianOptimization(game_runner_evaluate,{'col1': (0,21),
-												  'col2': (0,21),
-												  'col3': (0,21),
-												  'col4': (0,21),
-												  'col5': (0,21),
-												  'col6': (0,21),
-												  'col7': (0,21),
-												  'col8': (0,21),
-												  'col9': (0,21),
-												  'col10': (0,21),
-												  'h_diff1': (0,21),
-												  'h_diff2': (0,21),
-												  'h_diff3': (0,21),
-												  'h_diff4': (0,21),
-												  'h_diff5': (0,21),
-												  'h_diff6': (0,21),
-												  'h_diff7': (0,21),
-												  'h_diff8': (0,21),
-												  'h_diff9': (0,21),
-												  'num_holes': (0,21),
+grBO = BayesianOptimization(game_runner_evaluate,{'col1': (0,1),
+												  'col2': (0,1),
+												  'col3': (0,1),
+												  'col4': (0,1),
+												  'col5': (0,1),
+												  'col6': (0,1),
+												  'col7': (0,1),
+												  'col8': (0,1),
+												  'col9': (0,1),
+												  'col10': (0,1),
+												  'h_diff1': (0,1),
+												  'h_diff2': (0,1),
+												  'h_diff3': (0,1),
+												  'h_diff4': (0,1),
+												  'h_diff5': (0,1),
+												  'h_diff6': (0,1),
+												  'h_diff7': (0,1),
+												  'h_diff8': (0,1),
+												  'h_diff9': (0,1),
+												  'num_holes': (0,1),
 												  })
 grBO.maximize(init_points = 5,  n_iter=15)
 
