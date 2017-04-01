6a8087556ecad95867800f50e37f3111974d98b2
Bili Xu <xubili93@gmail.com>
2017-04-01 20:46:08 +0800

    Clean up swarm optimization code

--- pso.py
+++ pso.py
@@ -3,8 +3,17 @@ from game_runner import GameRunner
 
 class PSOLearner(object):
 
-    runner = GameRunner()
-    min_cleared = 0
+    # Static constants
+    UPPER_BOUND = 10
+    LOWER_BOUND = -10
+    OMEGA = 0.75
+    SWARMSIZE = 500
+    MAX_ITER = 10000
+    SHOW_DEBUG = False
+
+    def __init__(self):
+        self.runner = GameRunner()
+        self.min_cleared = 0
 
     # x is a vector of features
     def utility(self, x):
@@ -12,23 +21,25 @@ class PSOLearner(object):
         args = [str(arg) for arg in x]
 
         # Lines cleared
-        # print(args)
         cleared = self.runner.run(args)
         if int(float(cleared)) > self.min_cleared:
             print(args)
             print(cleared)
+            self.min_cleared = cleared
 
+        # Return negative because we want to maximize this value
         return -cleared
 
     def learn(self):
         # Lower bound
-        lb = [-100] * 11
+        lb = [self.LOWER_BOUND] * 11
 
         # Upper bound
-        ub = [100] * 11
+        ub = [self.UPPER_BOUND] * 11
 
         xopt, fopt = pso(self.utility, lb, ub,
-                         maxiter=10000)
+                         omega=self.OMEGA, swarmsize=self.SWARMSIZE,
+                         maxiter=self.MAX_ITER, debug=self.SHOW_DEBUG)
 
         print(xopt)
         print(fopt)
@@ -36,4 +47,3 @@ class PSOLearner(object):
 if __name__ == '__main__':
     learner = PSOLearner()
     learner.learn()
-
